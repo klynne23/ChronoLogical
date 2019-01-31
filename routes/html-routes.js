dc9ff1c8f4a29a181b7cd285e8ca488800cb4b38
ALP <alpetteruti@gmail.com>
2019-01-31 15:01:02 -0500

    logout

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -19,10 +19,9 @@ module.exports = function (app) {
        res.render("index");
       });
 
-
-    app.get("/", function(req, res){
-        res.render("mainpage"); 
-    })
+    //   app.get("/", function(req, res){
+    //     res.render("mainpage"); 
+    // })
 
     //redirects to create page if login credentials are authenticated
     app.get("/create", isAuthenticated, function(req, res){
